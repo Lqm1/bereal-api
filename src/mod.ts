645501db6fcc9eac32de11157c077133f7b87474
c7e715d1b04b17683718fb1e8944cc28 <ikki17617@gmail.com>
2025-05-19 00:24:07 +0900

    ♻️ リファクタリング(mod.ts): エクスポート内容の整理
    いくつかの定数と関数を`mod.ts`からエクスポートするように変更しました。これにより、モジュールの使いやすさが向上し、コードの保守性が向上します。

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,13 @@
 import { BeRealAuthVonage, BeRealAuth } from "./auth-l7.ts";
 import { BeReal } from "./mobile-l7.ts";
+import { BEREAL_DEFAULT_HEADERS, BEREAL_CLIENT_SECRET } from "./constants.ts";
+import { createBeRealSignature } from "./utils.ts";
 
-export { BeRealAuthVonage, BeRealAuth, BeReal };
+export {
+  BeRealAuthVonage,
+  BeRealAuth,
+  BeReal,
+  BEREAL_DEFAULT_HEADERS,
+  BEREAL_CLIENT_SECRET,
+  createBeRealSignature,
+};
